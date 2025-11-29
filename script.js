909b77d737bd2be0fa05b1200071ff99db3fad76
FELIPE ALVES FEZZI <86987347+felipefezzi1978@users.noreply.github.com>
2025-11-29 19:28:57 -0300

    Troca de imagens quando clicado nos botões

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const html = document.querySelector('html');
 const focoBotao = document.querySelector('.app__card-button--foco');
 const curtoBotao = document.querySelector('.app__card-button--curto');
 const longoBotao = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');    
 
 focoBotao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png');
 });
+
 curtoBotao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
 });
 longoBotao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png');
 });
